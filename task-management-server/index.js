2a82eee7a70ad8f820f444ae8eb4860ea537f750
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-02-19 23:21:13 -0500

    added user

--- task-management-server/index.js
+++ task-management-server/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const app = express();
 
@@ -10,6 +12,52 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kvlax.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const db = client.db('taskManagementDB')
+        const usersCollection = db.collection('users');
+        const tasksCollection = db.collection('tasks');
+
+        //adding user in DB
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const existingUser = await usersCollection.findOne({ email: user.email })
+
+            if (existingUser) {
+                return res.status(409).send({ message: 'User already exists' })
+            }
+
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+
+
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello from Task Management Server')
